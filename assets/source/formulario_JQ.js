e42c3189329b20752214ccfa752a22859dd0f3fa
pablo <pablo_mix_16@hotmail.com>
2023-04-08 17:36:33 -0300

    form

---
+++ assets/source/formulario_JQ.js
@@ -0,0 +1,33 @@
+$("#contacto-formulario").validate({
+  rules: {
+    nombre: {
+      required: true,
+      minlength: 3,
+      maxlength: 30,
+    },
+    email: {
+      required: true,
+      email: true,
+    },
+    telefono: {
+      required: true,
+    },
+    motivo: {
+      required: true,
+    },
+    mensaje: {
+      required: true,
+      minlength: 5,
+      maxlength: 200,
+    },
+  },
+});
+
+$("#guardar").click(function () {
+  let nombre = $("#nombre").val();
+  let email = $("#email").val();
+  let telefono = $("#telefono").val();
+  let motivo = $("#motivo").val();
+  let mensaje = $("#mensaje").val();
+  let terminos = $("#terminos").is(":checked");
+});
